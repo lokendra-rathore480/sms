1b3c42b901b68a3c30a600e83ceb7b5b2c6d41f6
lokendra-rathore480 <rlokendra480@gmail.com>
2026-05-18 13:00:38 +0530

    created RestApis

--- sms/src/main/java/com/example/sms/controller/UserController.java
+++ sms/src/main/java/com/example/sms/controller/UserController.java
@@ -1,10 +1,29 @@
 package com.example.sms.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.sms.dto.UserDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
 
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(UserDTO userDTO) {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(Long id) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUserById(Long id) {
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUserById(Long id, UserDTO userDTO) {
+        return null;
+    }
 }
