92c63162d0c97b29edbc39d498cc5a23e651d0b9
lokendra-rathore480 <rlokendra480@gmail.com>
2026-05-18 14:51:42 +0530

    UserService method implemented

--- sms/src/main/java/com/example/sms/controller/UserController.java
+++ sms/src/main/java/com/example/sms/controller/UserController.java
@@ -1,29 +1,35 @@
 package com.example.sms.controller;
 
 import com.example.sms.dto.UserDTO;
+import com.example.sms.service.IUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
 public class UserController {
 
+    private final IUserService userService;
     @PostMapping
-    public ResponseEntity<UserDTO> createUser(UserDTO userDTO) {
-        return null;
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
+        return ResponseEntity.ok(userService.createUser(userDTO));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserDTO> getUserById(Long id) {
-        return null;
+    public ResponseEntity<UserDTO> getUserById(@PathVariable("id")Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
     }
 
     @DeleteMapping("/{id}")
-    public void deleteUserById(Long id) {
+    public void deleteUserById(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<UserDTO> updateUserById(Long id, UserDTO userDTO) {
-        return null;
+    public ResponseEntity<UserDTO> updateUserById(@PathVariable("id")Long id, @RequestBody UserDTO userDTO) {
+        return ResponseEntity.ok(userService.updateUser(id, userDTO));
+
     }
 }
--- sms/src/main/java/com/example/sms/repository/UserRepository.java
+++ sms/src/main/java/com/example/sms/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.example.sms.repository;
 
-import com.example.sms.entity.Users;
+import com.example.sms.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Users, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
 }
