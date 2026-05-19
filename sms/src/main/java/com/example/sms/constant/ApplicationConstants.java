029537e3e025edfbaf49b098a1ec59ec876a484e
lokendra-rathore480 <rlokendra480@gmail.com>
2026-05-19 15:05:00 +0530

    added application constant class

---
+++ sms/src/main/java/com/example/sms/constant/ApplicationConstants.java
@@ -0,0 +1,7 @@
+package com.example.sms.constant;
+
+public class ApplicationConstants {
+
+    public static final String EMAIl_ALREADY_EXIST = "Email already exist";
+    public static final String USERNAME_ALREADY_EXIST = "Username already exist";
+}
